c0be63390f1fa6d403b9f887dcec075f8e7396d6
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2018-01-28 12:54:31 +0000

    feat: add trimming

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var extract = require('pdf-text-extract')
 // Usage: node index.js input.pdf -o output.pdf
 var filePath = path.join(__dirname, argv._[0])
 var outPath = path.join(__dirname, argv.o)
+var trimEnd = argv.t || 0; // Use flag -te <number> to supply number of characters from end of page to trim
+var verbose = argv.v || false;
+
+log("Verbose mode enabled.")
+log("Input file: " + filePath)
+log("Output file: " + outPath)
+log("Characters trimmed: " + trimEnd)
 
 extract(filePath, function (err, pages) {
   if (err) {
@@ -16,18 +23,20 @@ extract(filePath, function (err, pages) {
 
   selected = []
   for(var i = 0; i < pages.length; i++) {
-    curr_words = pages[i].split(' ')
-    curr = parseInt(curr_words[curr_words.length - 1].replace('\n', ''))
+    curr = extractPageNumber(pages[i])
 
     if(i + 1 < pages.length) {
-      next_words = pages[i + 1].split(' ')
-      next = parseInt(next_words[next_words.length - 1].replace('\n', ''))
-      if(curr < next)
+      next = extractPageNumber(pages[i + 1])
+
+      if(curr < next) {
         selected.push(i + 1)
+      }
     } else {
       selected.push(i + 1)
     }
   }
+
+  log("Selected slides: " + selected)
 
   var pdf = scissors(filePath)
     .pages(selected)
@@ -39,3 +48,15 @@ extract(filePath, function (err, pages) {
        throw err;
     });
 })
+
+function extractPageNumber(page) {
+  var contents = page.substring(0, page.length - trimEnd)
+  var curr_words = contents.split(' ')
+  var curr = curr_words[curr_words.length - 1].replace('\n', '')
+  return parseInt(curr);
+}
+
+function log(message) {
+  if(verbose)
+    console.log(message)
+}
